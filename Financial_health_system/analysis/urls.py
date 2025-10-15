f6776985b27ce17ade102b54a667a13cd2348e59
Sachin Gunawardena <56478025+sachin-gun@users.noreply.github.com>
2025-10-15 22:38:37 +0530

    Delete Financial_health_system directory

--- Financial_health_system/analysis/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("upload/", views.upload_pnl, name="upload_pnl"),
-    path("dashboard/", views.dashboard, name="dashboard"),
-]
